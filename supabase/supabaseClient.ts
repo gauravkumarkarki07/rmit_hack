790ec1da22862cc98ef4abb7ed1cf707048f51c0
Chakit Bhandari <chakitrocks@192-168-1-123.tpgi.com.au>
2024-09-21 01:47:58 +1000

    fix: removed dotenv package

--- supabase/supabaseClient.ts
+++ supabase/supabaseClient.ts
@@ -1,11 +1,7 @@
 // supabase/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
-// import {config} from 'dotenv';
-
-// config();
 
 const supabaseUrl:string = import.meta.env.VITE_SUPABASE_URL!;
 const supabaseAnonKey:string = import.meta.env.VITE_SUPABASE_ANON_KEY!;
-// console.log(supabaseAnonKey)
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
