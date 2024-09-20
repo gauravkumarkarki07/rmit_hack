4edb3cd2a4934e9778c73354e2f77b343ff8838e
Kulsgam <kuls.gam@gmail.com>
2024-09-20 23:36:59 +1000

    feat(stt): basic code structure

---
+++ src/Groq/stt.ts
@@ -0,0 +1,19 @@
+const API_ENDPOINT = "https://api.groq.com/openai/v1/audio/transcriptions";
+const MODEL_ID = "distil-whisper-large-v3-en";
+
+/* Accepts mp3, mp4, mpeg, mpga, m4a, wav, webm with max file size 25 MB*/
+export async function stt(
+    fileOrAudioBlob: File | Blob
+): Promise<string | null> {
+    if (fileOrAudioBlob instanceof File) {
+        return whisperStt(fileOrAudioBlob);
+    }
+
+    return whisperStt(
+        new File([fileOrAudioBlob], "audio.wav", { type: "audio/wav" })
+    );
+}
+
+async function whisperStt(file: File): Promise<string | null> {
+    return new Promise(() => file.name);
+}
