4f9d336eba081aab96ff5274e49f58afa9a31ced
Kulsgam <kuls.gam@gmail.com>
2024-09-21 17:56:39 +1000

    feat(coherent): tested implementation

--- src/Groq/__tests__/coherent.ts
+++ src/Groq/__tests__/coherent.ts
@@ -3,8 +3,30 @@ const Coherent = require("../Coherent").default;
 describe("Coherent function", () => {
     it("Should return a string for the input`", async () => {
         const coherent = new Coherent();
-        const response = await coherent.get("Hello");
+        let response = await coherent.get(
+            "I'm wondering how 1 + 1 is 2 it makes no sense bruh like how tell me how?!?!"
+        );
 
-        expect(typeof response).toBe("string");
+        // console.log("first response");
+
+        // console.log(response);
+
+        response = await coherent.get(
+            "But still bro how in the fuck I still don't understand help me broooooo please!!!"
+        );
+
+        // console.log("second response");
+
+        // console.log(response);
+
+        response = await coherent.get(
+            "Please explain bro I still don't understand"
+        );
+
+        // console.log("third response");
+
+        // console.log(response);
+
+        expect(typeof response).toBe("object");
     });
 });
